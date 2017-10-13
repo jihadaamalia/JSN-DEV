f3bd5061b07a9ffd18ed047f708e72a5fe5a5b43
jihadaa <jihada.amalia@ralali.com>
2017-10-13 21:32:04 +0700

    upload initial data

---
+++ home.php
@@ -0,0 +1,151 @@
+<html>
+	<head>
+		<title>Home</title> 
+
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <meta name="description" content="">
+	    <meta name="author" content="">
+
+		<!--CSS -->
+    	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    	<link rel="stylesheet" href="stylesheet/main.css">
+    	
+    	<!--JS-->
+    	<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+	</head>
+
+	<body>
+		<header>
+			<section  class="container">
+			  	<div class="row">
+					<div class="col-md-12 pull-right ">
+						<nav class="navbar navbar-toggleable-md">
+							<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+								<span class="navbar-toggler-icon"></span>
+							</button>
+						  	<a class="navbar-brand" href="#"><img src="asset/images/university-graduate-hat.png" width="50px;" /></a>
+						  	<div class="navbar-collapse collapse dual-collapse underline-nav" id="navbarNav">
+						        <ul class="navbar-nav ml-auto">
+						            <li class="nav-item">
+						                <a class="nav-link" href="#">Home</a>
+						                <span></span>
+						            </li>
+						            <li class="nav-item">
+						                <a class="nav-link" href="#">Universities</a>
+						                <span></span>
+						            </li>
+						        </ul>
+						    </div>
+						</nav>
+					</div> 
+				</div>
+				
+				<div class="row d-sm-flex justify-content-center" style="margin:200px 0;">
+					<form class="search-home form-inline">
+					  <label class="sr-only" for="inlineFormInput">University Name</label>
+					  <input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="University Name">
+
+					  <label class="sr-only" for="inlineFormInput">Region</label>
+					  <input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Region">
+
+					  <label class="sr-only" for="inlineFormInput">Major</label>
+					  <input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Major">
+					  
+					  <button type="submit" class="btn btn-primary">Find</button>
+					</form>
+				</div>
+			</section>
+		</header>
+
+		<main>
+			<section class="container">
+				<h2>Popular Universities</h2>
+				<br>
+				<div class="row">
+					<div class="col-md-3">
+						<div class="card">
+						  <img class="card-img-top" src="http://www.canberra.edu.au/__data/assets/image/0018/63441/ucic03.jpg" alt="Card image cap">
+						  <div class="card-block">
+						    <h4 class="card-title">Bina Nusantara</h4>
+						    <a href="#" class="btn btn-primary d-sm-flex ">Go somewhere</a>
+						  </div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="card">
+						  <img class="card-img-top" src="http://www.canberra.edu.au/__data/assets/image/0018/63441/ucic03.jpg" alt="Card image cap">
+						  <div class="card-block">
+						    <h4 class="card-title">Multimedia</h4>
+						    <a href="#" class="btn btn-primary d-sm-flex ">Go somewhere</a>
+						  </div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="card">
+						  <img class="card-img-top" src="http://www.canberra.edu.au/__data/assets/image/0018/63441/ucic03.jpg" alt="Card image cap">
+						  <div class="card-block">
+						    <h4 class="card-title">Trisakti</h4>
+						    <a href="#" class="btn btn-primary d-sm-flex ">Go somewhere</a>
+						  </div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="card">
+						  <img class="card-img-top" src="http://www.canberra.edu.au/__data/assets/image/0018/63441/ucic03.jpg" alt="Card image cap">
+						  <div class="card-block">
+						    <h4 class="card-title">Atmajaya</h4>
+						    <a href="#" class="btn btn-primary d-sm-flex ">Go somewhere</a>
+						  </div>
+						</div>
+					</div>
+				</div>
+			</section>
+			
+			<section class="container">
+				<br>
+				<br>
+				<h2>Popular Major</h2>
+				<br>
+				<div class="row">
+					<div class="col-md-3">
+						<div class="card text-center">
+						  <div class="card-block">
+						    <h4 class="card-title" style="padding: 80px 20px;">Special title treatment</h4>
+						  </div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="card text-center">
+						  <div class="card-block">
+						    <h4 class="card-title" style="padding: 80px 20px;">Special title treatment</h4>
+						  </div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="card text-center">
+						  <div class="card-block">
+						    <h4 class="card-title" style="padding: 80px 20px;">Special title treatment</h4>
+						  </div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="card text-center">
+						  <div class="card-block">
+						    <h4 class="card-title" style="padding: 80px 20px;">Special title treatment</h4>
+						  </div>
+						</div>
+					</div>
+					
+				</div>
+			</section>
+		</main>
+
+		<footer>
+		</footer>
+	</body>
+</html>
