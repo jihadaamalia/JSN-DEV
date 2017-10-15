463c317cdb92fbc13b8c0b512c24c0a613748798
nadiahaulia <nadiahnurul.na@gmail.com>
2017-10-15 23:05:08 +0700

    first front end

---
+++ search.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="asset/images/Untitled-1.png">
+
+    <title>Search Universitas</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="dist/css/css.css" rel="stylesheet">
+    <script src="dist/js/modernizr.custom.js"></script>
+
+  </head>
+
+  <body>
+<!-- header -->
+    <nav class="navbar navbar-light" style="background-color: #1e656d;">
+      <div class="input-group">
+        <a class="navbar-brand" href="home.php">
+          <img src="asset/images/Untitled-1.png" width="50" height="50" alt="">
+        </a>
+        <input type="text" class="form-control" placeholder="Search">
+        <span class="input-group-btn">
+          <button class="btn btn-secondary" type="button">Go</button>
+        </span>
+      </div>
+      <div class="row">
+      <div class="dropdown" style="margin:15px; width:300px;">
+        <button class="btn btn-secondary dropdown-toggle" style="width:300px;" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Universitas  </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item" href="#">Universitas Binus</a>
+          <a class="dropdown-item" href="#">Universitas Trisaksi</a>
+          <a class="dropdown-item" href="#">Universitas Tarumanegara</a>
+        </div>
+      </div>
+      <div class="dropdown" style="margin:15px; width:300px;">
+        <button class="btn btn-secondary dropdown-toggle" style="width:300px;" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Jurusan  </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item" href="#">Universitas Binus</a>
+          <a class="dropdown-item" href="#">Universitas Trisaksi</a>
+          <a class="dropdown-item" href="#">Universitas Tarumanegara</a>
+        </div>
+      </div>
+      </div>
+    </nav>
+  <!-- header -->
+<!-- 
+    <section class="jumbotron text-center">
+      <div class="container">
+        <h1 class="jumbotron-heading">Album example</h1>
+        <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
+        <p>
+          <a href="#" class="btn btn-primary">Main call to action</a>
+          <a href="#" class="btn btn-secondary">Secondary action</a>
+        </p>
+      </div>
+    </section>
+ -->
+      <div class="container demo-3">
+
+        <ul class="grid cs-style-3">
+        <li>
+          <figure>
+            <img src="asset/images/binus.png" alt="img04">
+            <figcaption>
+              <h5>Universitas Binus</h5>
+              <span>Jacob Cummings</span>
+              <a href="view.php">Selengkapnya</a>
+            </figcaption>
+          </figure>
+        </li>
+        <li>
+          <figure>
+            <img src="asset/images/1.png" alt="img01">
+            <figcaption>
+              <h5>Universitas Tarumanegara</h5>
+              <span>Jacob Cummings</span>
+              <a href="view.php">Selengkapnya</a>
+            </figcaption>
+          </figure>
+        </li>
+        <li>
+          <figure>
+            <img src="asset/images/2.png" alt="img02">
+            <figcaption>
+              <h5>Univeristas Trisakti</h5>
+              <span>Jacob Cummings</span>
+              <a href="view.php">Selengkapnya</a>
+            </figcaption>
+          </figure>
+        </li>
+        <li>
+          <figure>
+            <img src="asset/images/5.png" alt="img05">
+            <figcaption>
+              <h3>Safari</h3>
+              <span>Jacob Cummings</span>
+              <a href="http://dribbble.com/shots/1116775-Safari">Take a look</a>
+            </figcaption>
+          </figure>
+        </li>
+        <li>
+          <figure>
+            <img src="asset/images/3.png" alt="img03">
+            <figcaption>
+              <h3>Phone</h3>
+              <span>Jacob Cummings</span>
+              <a href="http://dribbble.com/shots/1117308-Phone">Take a look</a>
+            </figcaption>
+          </figure>
+        </li>
+        <li>
+          <figure>
+            <img src="asset/images/6.png" alt="img06">
+            <figcaption>
+              <h3>Game Center</h3>
+              <span>Jacob Cummings</span>
+              <a href="http://dribbble.com/shots/1118904-Game-Center">Take a look</a>
+            </figcaption>
+          </figure>
+        </li>
+      </ul>
+
+      </div>
+
+    <footer class="text-muted">
+      <div class="container">
+        <p class="float-right">
+          <a href="#">Back to top</a>
+        </p>
+        <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+        <p>New to Bootstrap? <a href="../../">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+
+    </script>
+    <script src="dist/js/toucheffects.js"></script>
+  </body>
+</html>
---
+++ view.php
@@ -0,0 +1,374 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="shortcut icon" href="asset/images/Untitled-1.png">
+
+  <title>Universitas Binus</title>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- Custom styles for this template -->
+    <link href="dist/css/css.css" rel="stylesheet">
+    <script src="dist/js/modernizr.custom.js"></script>
+  <style>
+  body {
+      font: 400 15px Lato, sans-serif;
+      line-height: 1.8;
+      color: #818181;
+  }
+
+  .logo-small {
+      color: #f4511e;
+      font-size: 50px;
+  }
+  .logo {
+      color: #f4511e;
+      font-size: 200px;
+  }
+
+
+  .item h4 {
+      font-size: 19px;
+      line-height: 1.375em;
+      font-weight: 400;
+      font-style: italic;
+      margin: 70px 0;
+  }
+  .item span {
+      font-style: normal;
+  }
+
+  .item img {
+    width: 100%;
+    height: 100%;
+
+  }
+
+  .navbar {
+      height:75px;
+      margin-bottom: 0;
+      background-color: #f7882f;
+      z-index: 9999;
+      border: 0;
+      font-size: 12px !important;
+      line-height: 1.42857143 !important;
+      letter-spacing: 4px;
+      border-radius: 0;
+      font-family: Montserrat, sans-serif;
+  }
+  
+
+  </style>
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="home.php">
+        <img src="asset/images/Untitled-1.png" width="50" height="50" alt="">
+      </a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#about">PROFILE</a></li>
+        <li><a href="#schedule">SCHEDULE</a></li>
+        <li><a href="#jalur">JALUR</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- Container (about Section) -->  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="asset/images/04.jpg" alt="Chania">
+      <div class="carousel-caption">
+        <h3>Chania</h3>
+        <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="asset/images/binus.png" alt="Chania">
+      <div class="carousel-caption">
+        <h3>Chania</h3>
+        <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="asset/images/binus.png" alt="Flower">
+      <div class="carousel-caption">
+        <h3>Flowers</h3>
+        <p>Beautiful flowers in Kolymbari, Crete.</p>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="asset/images/binus.png" alt="Flower">
+      <div class="carousel-caption">
+        <h3>Flowers</h3>
+        <p>Beautiful flowers in Kolymbari, Crete.</p>
+      </div>
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<!-- Container (About Section) -->
+<div id="about" class="container"><br><br><br>
+    <div class="col-sm-8">
+      <h2>About Company Page</h2><br>
+      <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h4><br>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+  <div class="row">
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-globe logo slideanim"></span>
+    </div>
+    <div class="col-sm-8">
+      <h2>Our Values</h2><br>
+      <h4><strong>MISSION:</strong> Our mission lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h4><br>
+      <p><strong>VISION:</strong> Our vision Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+  </div>
+</div>
+<!-- Container (About Section) -->
+
+<hr> 
+
+<!-- Container (Schedule Section) -->
+<div id="schedule" class="container-fluid text-center">
+<div class="main">
+  <div class="text-center">
+    <h2>SCHEDULE</h2>
+    <h4>Choose a payment plan that works for you</h4>
+  </div>
+    <section id="cd-timeline" class="cd-container slideanim">
+    <div class="cd-timeline-block slideanim">
+      <div class="cd-timeline-img">
+        <!-- <img src="img/cd-icon-picture.svg" alt="Picture"> -->
+      </div> <!-- cd-timeline-img -->
+
+      <div class="cd-timeline-content">
+        <h2>Title of section 1</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
+        <span class="cd-date">Jan 14</span>
+      </div> <!-- cd-timeline-content -->
+    </div> <!-- cd-timeline-block -->
+
+    <div class="cd-timeline-block slideanim">
+      <div class="cd-timeline-img"><!-- 
+        <img src="img/cd-icon-movie.svg" alt="Movie"> -->
+      </div> <!-- cd-timeline-img -->
+
+      <div class="cd-timeline-content">
+        <h2>Title of section 2</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde?</p>
+        <span class="cd-date">Jan 18</span>
+      </div> <!-- cd-timeline-content -->
+    </div> <!-- cd-timeline-block -->
+
+    <div class="cd-timeline-block slideanim">
+      <div class="cd-timeline-img">
+        <!-- <img src="img/cd-icon-picture.svg" alt="Picture"> -->
+      </div> <!-- cd-timeline-img -->
+
+      <div class="cd-timeline-content">
+        <h2>Title of section 3</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. .</p>
+        <span class="cd-date">Jan 24</span>
+      </div> <!-- cd-timeline-content -->
+    </div> <!-- cd-timeline-block -->
+
+    <div class="cd-timeline-block slideanim">
+      <div class="cd-timeline-img">
+        <!-- <img src="img/cd-icon-location.svg" alt="Location"> -->
+      </div> <!-- cd-timeline-img -->
+
+      <div class="cd-timeline-content">
+        <h2>Title of section 4</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
+        <span class="cd-date">Feb 14</span>
+      </div> <!-- cd-timeline-content -->
+    </div> <!-- cd-timeline-block -->
+
+    <div class="cd-timeline-block slideanim">
+      <div class="cd-timeline-img">
+        <!-- <img src="img/cd-icon-location.svg" alt="Location"> -->
+      </div> <!-- cd-timeline-img -->
+
+      <div class="cd-timeline-content">
+        <h2>Title of section 5</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum.</p>
+        <span class="cd-date">Feb 18</span>
+      </div> <!-- cd-timeline-content -->
+    </div> <!-- cd-timeline-block -->
+
+    <div class="cd-timeline-block slideanim">
+      <div class="cd-timeline-img cd-movie">
+        <img src="img/cd-icon-movie.svg" alt="Movie">
+      </div> <!-- cd-timeline-img -->
+
+      <div class="cd-timeline-content">
+        <h2>Final Section</h2>
+        <p>This is the content of the last section</p>
+        <span class="cd-date">Feb 26</span>
+      </div> <!-- cd-timeline-content -->
+    </div> <!-- cd-timeline-block -->
+  </section> <!-- cd-timeline -->
+</div>
+</div>
+
+<!-- Container (Jalur Section) -->
+<div id="jalur" class="container-fluid">
+  <div class="text-center">
+    <h2>JALUR</h2>
+    <h4>Choose a payment plan that works for you</h4>
+  </div>
+  <div class="row slideanim">   
+    <div class="col-sm-6 col-xs-12">
+      <div class="panel panel-default text-center">
+        <div class="panel-heading">
+          <h1>Beasiswa</h1>
+        </div>
+        <div class="panel-body"  style="overflow: auto; height: 300px;">
+          <p><strong>100</strong> Lorem</p>
+          <p><strong>50</strong> Ipsum</p>
+          <p><strong>25</strong> Dolor</p>
+          <p><strong>10</strong> Sit</p>
+          <p><strong>Endless</strong> Amet</p>
+          <p><strong>100</strong> Lorem</p>
+          <p><strong>50</strong> Ipsum</p>
+          <p><strong>25</strong> Dolor</p>
+          <p><strong>10</strong> Sit</p>
+          <p><strong>Endless</strong> Amet</p>
+          <p><strong>100</strong> Lorem</p>
+          <p><strong>50</strong> Ipsum</p>
+          <p><strong>25</strong> Dolor</p>
+          <p><strong>10</strong> Sit</p>
+          <p><strong>Endless</strong> Amet</p>
+        </div>
+        <div class="panel-footer">
+          <button class="btn btn-lg">Download Form</button>
+        </div>
+      </div>      
+    </div>       
+    <div class="col-sm-6 col-xs-12">
+      <div class="panel panel-default text-center">
+        <div class="panel-heading">
+          <h1>Mandiri</h1>
+        </div>
+        <div class="panel-body"  style="overflow: auto; height: 300px;">
+          <p><strong>100</strong> Lorem</p>
+          <p><strong>50</strong> Ipsum</p>
+          <p><strong>25</strong> Dolor</p>
+          <p><strong>10</strong> Sit</p>
+          <p><strong>Endless</strong> Amet</p>
+          <p><strong>100</strong> Lorem</p>
+          <p><strong>50</strong> Ipsum</p>
+          <p><strong>25</strong> Dolor</p>
+          <p><strong>10</strong> Sit</p>
+          <p><strong>Endless</strong> Amet</p>
+          <p><strong>100</strong> Lorem</p>
+          <p><strong>50</strong> Ipsum</p>
+          <p><strong>25</strong> Dolor</p>
+          <p><strong>10</strong> Sit</p>
+          <p><strong>Endless</strong> Amet</p>
+        </div>
+        <div class="panel-footer">
+          <button class="btn btn-lg">Daftar</button>
+        </div>
+      </div>      
+    </div>    
+  </div>
+</div>
+
+<footer class="container-fluid text-center">
+  <a href="#myPage" title="To Top">
+    <span class="glyphicon glyphicon-chevron-up"></span>
+  </a>
+  <!-- <div class="container">
+    <img src="">
+  </div> -->
+
+  <div class="row">
+    <div class="container">
+      <h3 style="letter-spacing:3px">COLLEGE REGISTRATION</h3>
+    </div>
+    <div class="container" style="padding:21px">
+      <a href="#about" class="item" style="text-decoration:none">Profile</a>
+      <a href="#schedule" class="item" style="text-decoration:none;padding: 0em 1.7em;">Schedule</a>
+      <a href="#jalur" class="item" style="text-decoration:none">Jalur</a>
+      <a href="#" class="item" style="text-decoration:none;padding: 0em 1.7em;">About Us</a>
+    </div>
+    <div class="container">
+    <img src="asset/images/logo WDC.png" style="width:100px; height: 100px; padding:10px;">
+    <img src="asset/images/logo HIMSISFO.png" style="width:100px; height: 100px; padding:10px;">
+    </div>
+  </div>
+  <p>Supported by JSN-Dev © 2017</p>
+</footer>
+
+<script>
+$(document).ready(function(){
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+    if (this.hash !== "") {      
+      event.preventDefault();
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+   
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+  
+  $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+          $(this).addClass("slide");
+        }
+    });
+  });
+})
+</script>
+
+</body>
+</html>
