070b960e85dc11cf2dc45f6aafe4912a5737c520
user <jihada.amalia@ralali.com>
2017-10-17 13:40:39 +0700

    add new section 'process'

--- home.php
+++ home.php
@@ -7,7 +7,17 @@
 	    <meta name="viewport" content="width=device-width, initial-scale=1">
 	    <meta name="description" content="">
 	    <meta name="author" content="">
-
+		<script>
+			function isElementInViewport(el) {
+				const rect = el.getBoundingClientRect();
+				return (
+					rect.top >= 0 &&
+					rect.left >= 0 &&
+					rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+					rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+				);
+			}
+		</script>
 		<?php require('head.php'); ?>
 
 	</head>
@@ -41,7 +51,7 @@
 					
 					<div class="headline" >
 						<h1 class="headline-text">APPLY NOW.</h1>
-						<p class="headline-description">Start your journey to the University with an Application for Admission</p>
+						<p class="headline-description">Start your journey to the University with an Application for Admission from a single web</p>
 
 						<form class="row justify-content-center main-search">
 						  	<div class="form-group">
@@ -78,7 +88,10 @@
 		<main>
 			<section>
 				<div class="container">
-					<h2 class="section-title">POPULAR UNIVERSITIES</h2>
+					<div class="row d-flex">
+						<div class="col-9"><h2 class="section-title">POPULAR UNIVERSITIES</h2></div>
+						<div class="col-3 d-flex"><h4 class="more align-self-end">Explore More <i class="fa fa-arrow-right" aria-hidden="true"></i></h4></div>
+					</div>
 					<ul class="grid cs-style-3">
 						<li>
 							<figure>
@@ -122,8 +135,10 @@
 			
 			<section>
 				<div class="container">
-					<h2 class="section-title">POPULAR MAJOR</h2>
-					
+					<div class="row d-flex">
+						<div class="col-9"><h2 class="section-title">POPULAR MAJORS</h2></div>
+						<div class="col-3 d-flex"><h4 class="more align-self-end">Explore More <i class="fa fa-arrow-right" aria-hidden="true"></i></h4></div>
+					</div>
 					<div class="row">
 						<div class="col-md-3">
 							<a 
@@ -141,7 +156,7 @@
 								class="caption" 
 								href="http://cargocollective.com/jaimemartinez/Illustration" 
 								data-title="MEDICAL" 
-								data-description="Undergraduate major for medical studies, requirement for doctor">
+								data-description="Undergraduate major for medical studies, required to be a doctor">
 
 								<img src="https://oup.silverchair-cdn.com/ImageLibrary/contact-medical.png" alt="Medical Major">
 								
@@ -152,7 +167,7 @@
 								class="caption" 
 								href="http://cargocollective.com/jaimemartinez/Illustration" 
 								data-title="ECONOMY" 
-								data-description="Field of major that requires lots of technical practive and strong quantitive skills">
+								data-description="Economics majors learn about economic theory, economic systems, and mathematical methods">
 
 								<img src="http://www.financenewspro.com/wp-content/uploads/2013/11/Economy.jpg" alt="Economy Major">
 								
@@ -163,7 +178,7 @@
 								class="caption" 
 								href="http://cargocollective.com/jaimemartinez/Illustration" 
 								data-title="IT" 
-								data-description="Field of major that requires lots of technical practive and strong quantitive skills">
+								data-description="Major that study computer science, business, and communications">
 
 								<img src="https://i.ndtvimg.com/i/2017-04/student-using-computer_650x400_41491999398.jpg" alt="IT">
 								
@@ -177,22 +192,59 @@
 			<section>
 				<div class="container">
 					<div class="row d-flex align-items-stretch">
-						<div class="d-flex single-web-section  col-6">
+						<div class="d-flex single-web-section col-md-6 col-xs-12">
 							<div class="align-self-center">
-								<h2 class="single-web-title">Create all the possibilities from a single web</h2>
+								<h2 class="single-web-title">Apply to multiple choices from a Single Web</h2>
 							</div>
 						</div>
-						<div class="col-6 padding-single-web">
+						<div class="col-md-6 col-xs-12 padding-single-web">
 							<p class="single-web-sub">Find</p>
-							<p class="single-web-desc">Use our univerity finder tool to explore all the university around Indonesia</p>
+							<p class="single-web-desc">Use our university finder tool to explore all the university around Indonesia</p>
 							<p class="single-web-sub">Read</p>
-							<p class="single-web-desc">Our extensive information for each university will help you go through all the selection process</p>
+							<p class="single-web-desc">Our extensive information for each university will help you understand all the selection process</p>
 							<p class="single-web-sub">Apply</p>
 							<p class="single-web-desc">Brace yourself and start your future by signing up</p>
 						</div>
 					
 					</div>
 				</div>
+			</section>
+
+			<section class="timeline">
+				<h2 class="call-text">How Do We Work?</h2>
+				<ol>
+					<li>
+						<div>
+							<time>Find Your University</time> Use our finder tool to find your dream university, open profile to see the informations.
+						</div>
+					</li>
+					<li>
+						<div>
+							<time>Choose Program</time> Select the selection program that you prefer from the available options.
+						</div>
+					</li>
+					<li>
+						<div>
+							<time>Fill in Your Data</time> Fill in all the required personal information that shown on the form.
+						</div>
+					</li>
+					<li>
+						<div>
+							<time>Download Form</time> Download the application form, fill in, and upload to our website.
+						</div>
+					</li>
+					<li>
+						<div>
+							<time>Transfer the Admission fee</time> Transfer the admission fee to the university bank account
+						</div>
+					</li>
+					<li>
+						<div>
+							<time>Prepare for the Test</time> You will receive entrance test card, and get ready for the entrance test based on given schedule.
+						</div>
+					</li>
+					<li></li>
+				</ol>
 			</section>
 
 			<section>
@@ -211,28 +263,22 @@
 		                    <h2 class="logo"><a href="#"> LOGO </a></h2>
 		                </div>
 		                <div class="col-sm-2">
-		                    <h5>Get started</h5>
+		                    <h5>Navigate</h5>
 		                    <ul>
 		                        <li><a href="#">Home</a></li>
-		                        <li><a href="#">Sign up</a></li>
-		                        <li><a href="#">Downloads</a></li>
+		                        <li><a href="#">University</a></li>
 		                    </ul>
 		                </div>
-		                <div class="col-sm-2">
-		                    <h5>About us</h5>
-		                    <ul>
-		                        <li><a href="#">Company Information</a></li>
-		                        <li><a href="#">Contact us</a></li>
-		                        <li><a href="#">Reviews</a></li>
-		                    </ul>
-		                </div>
-		                <div class="col-sm-2">
-		                    <h5>Support</h5>
-		                    <ul>
-		                        <li><a href="#">FAQ</a></li>
-		                        <li><a href="#">Help desk</a></li>
-		                        <li><a href="#">Forums</a></li>
-		                    </ul>
+		                <div class="col-sm-4">
+		                    <h5>Supported By</h5>
+							<div class="row">
+								<div class="col-sm-6">
+									<img src="asset/images/logo WDC.png" style="width:100px; height: 100px; padding:10px;">
+								</div>
+								<div class="col-sm-6">
+									<img src="asset/images/logo HIMSISFO.png" style="width:100px; height: 100px; padding:10px;">
+								</div>
+							</div>
 		                </div>
 		                <div class="col-sm-3">
 		                    <div class="social-networks">
@@ -240,12 +286,12 @@
 		                        <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
 		                        <a href="#" class="google"><i class="fa fa-google-plus"></i></a>
 		                    </div>
-		                    <button type="button" class="btn btn-default">Contact us</button>
+		                    <button href="mailto:contact@jsn-dev.com?subject=Information%2E%2E%2E%20%28DD%2FMM%2FYYYY%29" type="button" class="btn btn-default">Contact us</button>
 		                </div>
 		            </div>
 		        </div>
 		        <div class="footer-copyright">
-		            <center><p>© 2016 Copyright Text </p></center>
+		            <center><p>© 2017 JSN Developers </p></center>
 		        </div>
 			</section>
 		</footer>
