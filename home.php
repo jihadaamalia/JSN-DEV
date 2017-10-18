26e4ac0c0ea5bf552b79e46cb3e21cb98958f1de
nadiahaulia <nadiahnurul.na@gmail.com>
2017-10-18 21:36:52 +0700

    commit 3

--- home.php
+++ home.php
@@ -8,144 +8,246 @@
 	    <meta name="description" content="">
 	    <meta name="author" content="">
 
-		<!--CSS -->
-    	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-    	<link rel="stylesheet" href="stylesheet/main.css">
-    	
-    	<!--JS-->
-    	<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+		<?php require('head.php'); ?>
 
 	</head>
 
 	<body>
 		<header>
-			<section  class="container">
-			  	<div class="row">
-					<div class="col-md-12 pull-right ">
-						<nav class="navbar navbar-toggleable-md">
-							<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-								<span class="navbar-toggler-icon"></span>
-							</button>
-						  	<a class="navbar-brand" href="#"><img src="asset/images/university-graduate-hat.png" width="50px;" /></a>
-						  	<div class="navbar-collapse collapse dual-collapse underline-nav" id="navbarNav">
-						        <ul class="navbar-nav ml-auto">
-						            <li class="nav-item">
-						                <a class="nav-link" href="#">Home</a>
-						                <span></span>
-						            </li>
-						            <li class="nav-item">
-						                <a class="nav-link" href="#">Universities</a>
-						                <span></span>
-						            </li>
-						        </ul>
+			<section>
+				<div class="container">
+				  	<div class="row">
+						<div class="col-md-12 pull-right ">
+							<nav class="navbar navbar-toggleable-md">
+								<button class="navbar-button navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+									<span class="navbar-toggler-icon"></span>
+								</button>
+							  	<a class="navbar-brand" href="#"><img src="asset/images/university-graduate-hat.png" width="50px;" /></a>
+							  	<div class="navbar-collapse collapse dual-collapse underline-nav" id="navbarNav">
+							        <ul class="navbar-nav ml-auto">
+							            <li class="nav-item">
+							                <a class="nav-link" href="#">Home</a>
+							                <span></span>
+							            </li>
+							            <li class="nav-item">
+							                <a class="nav-link" href="#">Universities</a>
+							                <span></span>
+							            </li>
+							        </ul>
+							    </div>
+							</nav>
+						</div> 
+					</div>
+					
+					<div class="headline" >
+						<h1 class="headline-text">APPLY NOW.</h1>
+						<p class="headline-description">Start your journey to the University with an Application for Admission</p>
+
+						<form class="row justify-content-center main-search">
+						  	<div class="form-group">
+						    	<label for="uni-name" class="sr-only">University Name</label>
+						    	<input type="text" class="form-control" id="uni-name" placeholder="University Name">
+						  	</div>
+						    <div class="form-group">
+								<select class="form-control" style=" height:100px; padding: 0 50px;">
+								<option value="" disabled selected style="display: none;">University Area</option>
+								  <option>1</option>
+								  <option>2</option>
+								  <option>3</option>
+								  <option>4</option>
+								  <option>5</option>
+								</select>
 						    </div>
-						</nav>
-					</div> 
-				</div>
-				
-				<div class="row d-sm-flex justify-content-center" style="margin:200px 0;">
-					<form class="search-home form-inline">
-					  <label class="sr-only" for="inlineFormInput">University Name</label>
-					  <input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="University Name">
-
-					  <label class="sr-only" for="inlineFormInput">Region</label>
-					  <input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Region">
-
-					  <label class="sr-only" for="inlineFormInput">Major</label>
-					  <input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Major">
-					  
-					  <button type="submit" class="btn btn-primary">Find</button>
-					</form>
+						    <div class="form-group">
+								<select class="form-control" style=" height:100px; padding: 0 50px;">
+								  	<option value="" disabled selected style="display: none;">Selected Major</option>
+								    <option>1</option>
+								    <option>2</option>
+								    <option>3</option>
+								    <option>4</option>
+								    <option>5</option>
+								</select>
+						    </div>
+						  	<button type="submit" class="btn btn-primary">Find</button>
+						</form>
+					</div>
 				</div>
 			</section>
 		</header>
 
 		<main>
-			<section class="container">
-				<h2>Popular Universities</h2>
-				<br>
-				<div class="row">
-					<div class="col-md-3">
-						<div class="card">
-						  <img class="card-img-top" src="http://www.canberra.edu.au/__data/assets/image/0018/63441/ucic03.jpg" alt="Card image cap">
-						  <div class="card-block">
-						    <h4 class="card-title">Bina Nusantara</h4>
-						    <a href="#" class="btn btn-primary d-sm-flex ">Go somewhere</a>
-						  </div>
+			<section>
+				<div class="container">
+					<h2 class="section-title">POPULAR UNIVERSITIES</h2>
+					<ul class="grid cs-style-3">
+						<li>
+							<figure>
+								<img src="asset/images/universitas/trisakti.jpg" alt="img02">
+								<figcaption>
+								  <h5>Universitas Trisakti</h5>
+								  <a class="d-flex justify-content-center" href="view.php">Selengkapnya</a>
+								</figcaption>
+							</figure>
+					    </li>
+						<li>
+							<figure>
+								<img src="asset/images/universitas/binus.jpg" alt="img02">
+								<figcaption>
+								  <h5>Universitas Bina Nusantara</h5>
+								  <a class="d-flex justify-content-center" href="view.php">Selengkapnya</a>
+								</figcaption>
+							</figure>
+					    </li>
+						<li>
+							<figure>
+								<img src="asset/images/universitas/umn.jpg" alt="img02">
+								<figcaption>
+								  <h5>Universitas Multimedia Nusantara</h5>
+								  <a class="d-flex justify-content-center" href="view.php">Selengkapnya</a>
+								</figcaption>
+							</figure>
+					    </li>
+						<li>
+							<figure>
+								<img src="asset/images/universitas/untar.jpg" alt="img02">
+								<figcaption>
+								  <h5>Universitas Tarumanegara</h5>
+								  <a class="d-flex justify-content-center" href="view.php">Selengkapnya</a>
+								</figcaption>
+							</figure>
+					    </li>
+					</ul>
+	            </div>
+			</section>
+			
+			<section>
+				<div class="container">
+					<h2 class="section-title">POPULAR MAJOR</h2>
+					
+					<div class="row">
+						<div class="col-md-3">
+							<a 
+								class="caption" 
+								href="http://cargocollective.com/jaimemartinez/Illustration" 
+								data-title="ENGINEERING" 
+								data-description="Field of major that requires lots of technical practice and strong quantitive skills">
+
+								<img src="https://www.myamcat.com/blog/wp-content/uploads/2017/07/Engineering.jpg" alt="Engineering Major">
+								
+							</a>
 						</div>
-					</div>
-					<div class="col-md-3">
-						<div class="card">
-						  <img class="card-img-top" src="http://www.canberra.edu.au/__data/assets/image/0018/63441/ucic03.jpg" alt="Card image cap">
-						  <div class="card-block">
-						    <h4 class="card-title">Multimedia</h4>
-						    <a href="#" class="btn btn-primary d-sm-flex ">Go somewhere</a>
-						  </div>
+						<div class="col-md-3">
+							<a 
+								class="caption" 
+								href="http://cargocollective.com/jaimemartinez/Illustration" 
+								data-title="MEDICAL" 
+								data-description="Undergraduate major for medical studies, requirement for doctor">
+
+								<img src="https://oup.silverchair-cdn.com/ImageLibrary/contact-medical.png" alt="Medical Major">
+								
+							</a>
 						</div>
-					</div>
-					<div class="col-md-3">
-						<div class="card">
-						  <img class="card-img-top" src="http://www.canberra.edu.au/__data/assets/image/0018/63441/ucic03.jpg" alt="Card image cap">
-						  <div class="card-block">
-						    <h4 class="card-title">Trisakti</h4>
-						    <a href="#" class="btn btn-primary d-sm-flex ">Go somewhere</a>
-						  </div>
+						<div class="col-md-3">
+							<a 
+								class="caption" 
+								href="http://cargocollective.com/jaimemartinez/Illustration" 
+								data-title="ECONOMY" 
+								data-description="Field of major that requires lots of technical practive and strong quantitive skills">
+
+								<img src="http://www.financenewspro.com/wp-content/uploads/2013/11/Economy.jpg" alt="Economy Major">
+								
+							</a>
 						</div>
-					</div>
-					<div class="col-md-3">
-						<div class="card">
-						  <img class="card-img-top" src="http://www.canberra.edu.au/__data/assets/image/0018/63441/ucic03.jpg" alt="Card image cap">
-						  <div class="card-block">
-						    <h4 class="card-title">Atmajaya</h4>
-						    <a href="#" class="btn btn-primary d-sm-flex ">Go somewhere</a>
-						  </div>
+						<div class="col-md-3">
+							<a 
+								class="caption" 
+								href="http://cargocollective.com/jaimemartinez/Illustration" 
+								data-title="IT" 
+								data-description="Field of major that requires lots of technical practive and strong quantitive skills">
+
+								<img src="https://i.ndtvimg.com/i/2017-04/student-using-computer_650x400_41491999398.jpg" alt="IT">
+								
+							</a>
 						</div>
 					</div>
 				</div>
+				<br><br><br>
 			</section>
-			
-			<section class="container">
-				<br>
-				<br>
-				<h2>Popular Major</h2>
-				<br>
-				<div class="row">
-					<div class="col-md-3">
-						<div class="card text-center">
-						  <div class="card-block">
-						    <h4 class="card-title" style="padding: 80px 20px;">Special title treatment</h4>
-						  </div>
+
+			<section>
+				<div class="container">
+					<div class="row d-flex align-items-stretch">
+						<div class="d-flex single-web-section  col-6">
+							<div class="align-self-center">
+								<h2 class="single-web-title">Create all the possibilities from a single web</h2>
+							</div>
 						</div>
-					</div>
-					<div class="col-md-3">
-						<div class="card text-center">
-						  <div class="card-block">
-						    <h4 class="card-title" style="padding: 80px 20px;">Special title treatment</h4>
-						  </div>
+						<div class="col-6 padding-single-web">
+							<p class="single-web-sub">Find</p>
+							<p class="single-web-desc">Use our univerity finder tool to explore all the university around Indonesia</p>
+							<p class="single-web-sub">Read</p>
+							<p class="single-web-desc">Our extensive information for each university will help you go through all the selection process</p>
+							<p class="single-web-sub">Apply</p>
+							<p class="single-web-desc">Brace yourself and start your future by signing up</p>
 						</div>
-					</div>
-					<div class="col-md-3">
-						<div class="card text-center">
-						  <div class="card-block">
-						    <h4 class="card-title" style="padding: 80px 20px;">Special title treatment</h4>
-						  </div>
-						</div>
-					</div>
-					<div class="col-md-3">
-						<div class="card text-center">
-						  <div class="card-block">
-						    <h4 class="card-title" style="padding: 80px 20px;">Special title treatment</h4>
-						  </div>
-						</div>
-					</div>
 					
+					</div>
+				</div>
+			</section>
+
+			<section>
+				<div class="container">
+					<center><h2 class="call-text">Sounds interesting?</h2>
+					<a class="call-button">TRY NOW</a></center>
 				</div>
 			</section>
 		</main>
 
 		<footer>
+			<section>
+		        <div id= "myFooter" class="container">
+		            <div class="row">
+		                <div class="col-sm-3">
+		                    <h2 class="logo"><a href="#"> LOGO </a></h2>
+		                </div>
+		                <div class="col-sm-2">
+		                    <h5>Get started</h5>
+		                    <ul>
+		                        <li><a href="#">Home</a></li>
+		                        <li><a href="#">Sign up</a></li>
+		                        <li><a href="#">Downloads</a></li>
+		                    </ul>
+		                </div>
+		                <div class="col-sm-2">
+		                    <h5>About us</h5>
+		                    <ul>
+		                        <li><a href="#">Company Information</a></li>
+		                        <li><a href="#">Contact us</a></li>
+		                        <li><a href="#">Reviews</a></li>
+		                    </ul>
+		                </div>
+		                <div class="col-sm-2">
+		                    <h5>Support</h5>
+		                    <ul>
+		                        <li><a href="#">FAQ</a></li>
+		                        <li><a href="#">Help desk</a></li>
+		                        <li><a href="#">Forums</a></li>
+		                    </ul>
+		                </div>
+		                <div class="col-sm-3">
+		                    <div class="social-networks">
+		                        <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+		                        <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+		                        <a href="#" class="google"><i class="fa fa-google-plus"></i></a>
+		                    </div>
+		                    <button type="button" class="btn btn-default">Contact us</button>
+		                </div>
+		            </div>
+		        </div>
+		        <div class="footer-copyright">
+		            <center><p>© 2016 Copyright Text </p></center>
+		        </div>
+			</section>
 		</footer>
 	</body>
 </html>
