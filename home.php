2a6fe739c97815a51410b6a1b537884c6beec667
jihadaa <jihada.amalia@ralali.com>
2017-10-20 22:08:42 +0700

    edit about card size, homepage icon

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 		<title>Home</title> 
-
+		<link rel="shortcut icon" href="asset/images/logo.png">
 		<meta charset="utf-8">
 	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	    <meta name="viewport" content="width=device-width, initial-scale=1">
