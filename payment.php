4e14f26c348078f6b770aa0397891dfc76ec71ba
SucianaLF <sucianalathifah@yahoo.com>
2017-10-20 20:59:57 +0700

    payment.php

---
+++ payment.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<title>Blueprint: Nested Accordion</title>
+	<meta name="description" content="Blueprint: Nested Accordion" />
+	<meta name="keywords" content="nested, accordion, simple, vertical, web development, css3, javascript" />
+	<meta name="author" content="Codrops" />
+	<link rel="shortcut icon" href="../favicon.ico">
+	<link rel="stylesheet" type="text/css" href="dist/css/default_payment.css" />
+	<link rel="stylesheet" type="text/css" href="dist/css/payment1.css" />
+	<script src="dist/js/modernizr.custom.js"></script>
+</head>
+<body>
+	<div class="container">
+		<header class="clearfix">
+
+			<h1 style="color:#F7882F; ">Payment Method</h1>
+
+		</header>	
+		<div class="main">
+			<ul id="cbp-ntaccordion" class="cbp-ntaccordion">
+				<li>
+					<h3 class="cbp-nttrigger">Bank Mandiri</h3>
+					<div class="cbp-ntcontent">
+						<ul class="cbp-ntsubaccordion">
+							<li>
+								<h4 class="cbp-nttrigger">ATM Mandiri</h4>
+								<div class="cbp-ntcontent">
+									<p>Payment methods using ATM Mandiri are as follows:<br>
+										1. Select the Payment / Purchase menu<br>
+										2. Select Multi Payment<br>
+										3. Enter the company code 10003 (UI) then press TRUE<br>
+										4. Enter the 9 digit registration number then press the TRUE button<br>
+										5. The screen will display the identity and payment amount; press 1 if the data matches<br>
+										6. To execute, press "YES", for cancellation press "NO"<br>
+									</p>
+								</div>
+							</li>
+							<li>
+								<h4 class="cbp-nttrigger">Teller Mandiri</h4>
+								<div class="cbp-ntcontent">
+									<p>Fill in the form of Multi Payment by stating the registration number and the name of the applicant for the purpose of payment of University<br>
+									Submit a blank to the teller to process the payment</p>
+								</div>
+							</li>
+							<li>
+								<h4 class="cbp-nttrigger">Internet Banking</h4>
+								<div class="cbp-ntcontent">
+									<p>Login with User ID and Password<br>
+										1. Select the Payment menu<br>
+										2. Select the Education menu<br>
+										3. Select the account to pay for<br>
+										4. Select Service Provider: 10003 Universitas<br>
+										5. Enter your registration number in the Student Number field<br>
+										6. Click "Continue", check the information that appears.<br>7. If appropriate, enter the PIN degenerate by Token into the available field. Select "Submit"<br>
+									A validation of the system, print or save appears to be used as evidence.</p>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</li>
+				<li>
+					<h3 class="cbp-nttrigger">CIMB Niaga</h3>
+					<div class="cbp-ntcontent">
+						<ul class="cbp-ntsubaccordion">
+							<li>
+								<h4 class="cbp-nttrigger">ATM CIMB Niaga</h4>
+								<div class="cbp-ntcontent">
+									<p>Payment methods using ATM Mandiri are as follows:<br>
+										1. Select the Payment / Purchase menu<br>
+										2. Select Multi Payment<br>
+										3. Enter the company code 10003 (UI) then press TRUE<br>
+										4. Enter the 9 digit registration number then press the TRUE button<br>
+										5. The screen will display the identity and payment amount; press 1 if the data matches<br>
+										6. To execute, press "YES", for cancellation press "NO"<br>
+									</p>
+								</div>
+							</li>
+							<li>
+								<h4 class="cbp-nttrigger">Teller CIMB Niaga</h4>
+								<div class="cbp-ntcontent">
+									<p>Fill in the form of Multi Payment by stating the registration number and the name of the applicant for the purpose of payment of University<br>
+									Submit a blank to the teller to process the payment</p>
+								</div>
+							</li>
+							<li>
+								<h4 class="cbp-nttrigger">Internet Banking</h4>
+								<div class="cbp-ntcontent">
+									<p>Login with User ID and Password<br>
+										1. Select the Payment menu<br>
+										2. Select the Education menu<br>
+										3. Select the account to pay for<br>
+										4. Select Service Provider: 10003 Universitas<br>
+										5. Enter your registration number in the Student Number field<br>
+										6. Click "Continue", check the information that appears.<br>7. If appropriate, enter the PIN degenerate by Token into the available field. Select "Submit"<br>
+									A validation of the system, print or save appears to be used as evidence.</p>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</li>
+				<li>
+					<h3 class="cbp-nttrigger">Bank BRI</h3>
+					<div class="cbp-ntcontent">
+						<ul class="cbp-ntsubaccordion">
+							<li>
+								<h4 class="cbp-nttrigger">ATM BRI</h4>
+								<div class="cbp-ntcontent">
+									<p>Payment methods using ATM Mandiri are as follows:<br>
+										1. Select the Payment / Purchase menu<br>
+										2. Select Multi Payment<br>
+										3. Enter the company code 10003 (UI) then press TRUE<br>
+										4. Enter the 9 digit registration number then press the TRUE button<br>
+										5. The screen will display the identity and payment amount; press 1 if the data matches<br>
+										6. To execute, press "YES", for cancellation press "NO"<br>
+									</p>
+								</div>
+							</li>
+							<li>
+								<h4 class="cbp-nttrigger">Teller BRI</h4>
+								<div class="cbp-ntcontent">
+									<p>Fill in the form of Multi Payment by stating the registration number and the name of the applicant for the purpose of payment of University<br>
+									Submit a blank to the teller to process the payment</p>
+								</div>
+							</li>
+							<li>
+								<h4 class="cbp-nttrigger">Internet Banking</h4>
+								<div class="cbp-ntcontent">
+									<p>Login with User ID and Password<br>
+										1. Select the Payment menu<br>
+										2. Select the Education menu<br>
+										3. Select the account to pay for<br>
+										4. Select Service Provider: 10003 Universitas<br>
+										5. Enter your registration number in the Student Number field<br>
+										6. Click "Continue", check the information that appears.<br>7. If appropriate, enter the PIN degenerate by Token into the available field. Select "Submit"<br>
+									A validation of the system, print or save appears to be used as evidence.</p>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</li>
+				<li>
+					<h3 class="cbp-nttrigger">Bank BNI</h3>
+					<div class="cbp-ntcontent">
+						<ul class="cbp-ntsubaccordion">
+							<li>
+								<h4 class="cbp-nttrigger">ATM BNI</h4>
+								<div class="cbp-ntcontent">
+									<p>Payment methods using ATM Mandiri are as follows:<br>
+										1. Select the Payment / Purchase menu<br>
+										2. Select Multi Payment<br>
+										3. Enter the company code 10003 (UI) then press TRUE<br>
+										4. Enter the 9 digit registration number then press the TRUE button<br>
+										5. The screen will display the identity and payment amount; press 1 if the data matches<br>
+										6. To execute, press "YES", for cancellation press "NO"<br>
+									</p>
+								</div>
+							</li>
+							<li>
+								<h4 class="cbp-nttrigger">Teller BNI</h4>
+								<div class="cbp-ntcontent">
+									<p>Fill in the form of Multi Payment by stating the registration number and the name of the applicant for the purpose of payment of University<br>
+									Submit a blank to the teller to process the payment</p>
+								</div>
+							</li>
+							<li>
+								<h4 class="cbp-nttrigger">Internet Banking</h4>
+								<div class="cbp-ntcontent">
+									<p>Login with User ID and Password<br>
+										1. Select the Payment menu<br>
+										2. Select the Education menu<br>
+										3. Select the account to pay for<br>
+										4. Select Service Provider: 10003 Universitas<br>
+										5. Enter your registration number in the Student Number field<br>
+										6. Click "Continue", check the information that appears.<br>7. If appropriate, enter the PIN degenerate by Token into the available field. Select "Submit"<br>
+									A validation of the system, print or save appears to be used as evidence.</p>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</li>
+</ul>
+</div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="dist/js/jquery.cbpNTAccordion.min.js"></script>
+<script>
+	$( function() {
+				/*
+				- how to call the plugin:
+				$( selector ).cbpNTAccordion( [options] );
+				- destroy:
+				$( selector ).cbpNTAccordion( 'destroy' );
+				*/
+
+				$( '#cbp-ntaccordion' ).cbpNTAccordion();
+
+			} );
+		</script>
+	</body>
+	</html>
