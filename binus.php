23f50c7eef9f4e22bc4ac5c2efd4126901b5d5c4
nadiahaulia <nadiahnurul.na@gmail.com>
2017-10-20 13:10:36 +0700

    1 page univ

---
+++ binus.php
@@ -0,0 +1,433 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="shortcut icon" href="asset/images/logo.png">
+
+  <title>Universitas Binus</title>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" href="stylesheet/main.css">
+    <link href="dist/css/css.css" rel="stylesheet">
+    <link href="dist/css/news.css" rel="stylesheet">
+    <script src="dist/js/modernizr.custom.js"></script>
+  <style>
+  body {
+      font: 400 15px Lato, sans-serif;
+      line-height: 1.8;
+      color: black;
+  }
+
+  .item h4 {
+      font-size: 19px;
+      line-height: 1.375em;
+      font-weight: 400;
+      font-style: italic;
+      margin: 70px 0;
+  }
+  .item span {
+      font-style: normal;
+  }
+
+  .item img {
+    width: 100%;
+    height: 100%;
+
+  }
+
+  .navbar {
+      height:75px;
+      margin-bottom: 0;
+      background-color: #f7c331;
+      z-index: 9999;
+      border: 0;
+      font-size: 12px !important;
+      line-height: 1.42857143 !important;
+      letter-spacing: 4px;
+      border-radius: 0;
+      font-family: Montserrat, sans-serif;
+  }
+  
+  .glyphicon.glyphicon-list-alt {
+    padding-top:15px;font-size: 25px;
+}
+
+  </style>
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="home.php">
+        <img src="asset/images/logo.png" width="50" height="50" alt="">
+      </a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#about">PROFILE</a></li>
+        <li><a href="#schedule">SCHEDULE</a></li>
+        <li><a href="#jalur">ADMISSION</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- Container (about Section) -->  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="asset/images/universitas/binus4.jpg" alt="Chania">
+    </div>
+
+    <div class="item">
+      <img src="asset/images/universitas/binus.png" alt="Chania">
+    </div>
+
+    <div class="item">
+      <img src="asset/images/universitas/Binus (1).png" alt="Flower">
+    </div>
+
+    <div class="item">
+      <img src="asset/images/universitas/binus2.jpg" alt="Flower">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<!-- Container (About Section) -->
+<div id="about" class="container"><br><br><br>
+  <div class="row"> 
+    <div class="col-sm-8">
+      <h1>History</h1><br>
+      <p>Universitas Bina Nusantara, atau juga dikenal dengan Binus University, merupakan salah satu universitas swasta yang berlokasi di Jakarta, Indonesia. Berawal dari sebuah institusi pelatihan komputer Modern Computer Course pada 21 Oktober 1974, dan berkembang dan akhirnya berubah menjadi Universitas pada 8 Agustus 1996, Universitas Bina Nusantara.<br>
+
+      Program kuliah, Fakultas dan program studi Universitas Bina Nusantara – Binus University: fakultas Ekonomi dan Komunikasi: Akuntansi, Keuangan, Manajemen Hotel (D4) serta Komunikasi Pemasaran. Fakultas Teknik: Teknik Industri, Arsitektur, Teknik Sipil serta Teknik Komputer. Sekolah Desain: Desain Komunikasi Visual Media Baru, Desain Interior, Desain Komunikasi Visual Animasi dan Desain Komunikasi Visual Periklanan Kreatif. Sekolah Ilmu Komputer: Teknik Informatika, Teknologi dan Aplikasi Bergerak (Mobile Application and Technology) serta Teknologi dan Aplikasi Permainan (Game Application and Technology).</p><br><br>
+
+      <h1>Our Values</h1><br>
+      <p><strong>VISION:</strong><br>“A World-class university <br>… In continuous pursuit of innovation and enterprise”</p>
+      <p><strong>MISSION:</strong><br>The mission of BINUS University is to contribute to the global community through the provision of world-class education by :<p<br>
+      <li>Recognizing and rewarding the most creative and value-adding talents</li>
+      <li>Providing a world-class teaching, learning and research experience that fosters excellence in scholarship, innovation and entrepreneurship.</li>
+      <li>Conducting professional services with an emphasis on application of knowledge to the society</li>
+      <li>Creating outstanding leaders for global community</li>
+      <li>Improving the quality of life of Indonesians and the international community</li><br>
+    </div>
+    <div class="col-sm-4">
+          
+    <div class="container-article">
+      <div class="info pull-left">
+        <h1>Article News</h1>
+      </div>
+      <!-- Normal Demo-->
+      <div class="column">
+        <!-- Post-->
+        <div class="post-module">
+          <!-- Thumbnail-->
+          <div class="thumbnail">
+            <div class="date">
+              <div class="day">27</div>
+              <div class="month">Mar</div>
+            </div><img src="asset/images/universitas/terbaik.jpg" style="height:60%;" />
+          </div>
+          <!-- Post Content-->
+          <div class="post-content">
+            <div class="category">Photos</div>
+            <h1 class="title">Perguruan Tinggi Terbaik</h1>
+            <h2 class="sub_title">Raih 7 Penghargaan</h2>
+            <p class="description">Binus University Raih 7 Penghargaan Perguruan Tinggi Swasta Terbaik 2015</p>
+          </div>
+        </div> <br>
+
+        <div class="post-module">
+          <!-- Thumbnail-->
+          <div class="thumbnail">
+            <div class="date">
+              <div class="day">27</div>
+              <div class="month">April</div>
+            </div><img src="asset/images/universitas/terbaik2.jpg"/>
+          </div>
+          <!-- Post Content-->
+          <div class="post-content">
+            <div class="category">Photos</div>
+            <h1 class="title">Mahasiswa/i Berprestasi</h1>
+            <h2 class="sub_title">Wisudawan Terbaik tahun 2017</h2>
+            <p class="description">Selamat kepada Mahasiswa Lulusan Terbaik Jurusan Sistem Informasi Wisuda 54 Binus University. Ki -ka : Selvi Nelin ( IPK:3.94); Inca Maya Sari (IPK:4.00, yang juga meraih Binusian Award dan Binus Award of Excellence); dan Junario Wibawa (IPK:3.94). #bestgraduates  #wisuda54  #sis  #sisteminformasi  #informationsystem  #sis  #GreaterNusantara  #summacumlaude  #schoolofis  #schollisbinus  #Binusian  #binus </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    </div>
+  </div>
+</div>
+<!-- Container (About Section) -->
+<br>
+
+<!-- Container (Schedule Section) -->
+<section style="background:url('asset/images/pattern.JPG'); background-repeat: no-repeat; background-attachment: fixed; background-size: cover;">
+<div id="schedule" class="container-fluid text-center">
+<div class="main">
+  <div class="text-center">
+    <h2>SCHEDULE</h2>
+    <h4>Choose a payment plan that works for you</h4>
+  </div>
+    <section id="cd-timeline" class="cd-container slideanim">
+    <div class="cd-timeline-block slideanim">
+      <div class="cd-timeline-img">
+        <i class="glyphicon glyphicon-list-alt"></i><!-- <img src="img/cd-icon-picture.svg" alt="Picture"> -->
+      </div> <!-- cd-timeline-img -->
+
+      <div class="cd-timeline-content">
+        <h2>Pendaftaran Jalur Mandiri</h2>
+        <li>Pendaftaran jalur mandiri melalui <a href="#">(www.jsn-dev/pendaftaran_mandiri) </a></li>
+        <span class="cd-date">16 Mei 2017 - 30 June 2017</span>
+      </div> <!-- cd-timeline-content -->
+    </div> <!-- cd-timeline-block -->
+
+    <div class="cd-timeline-block slideanim">
+      <div class="cd-timeline-img">
+      <i class="glyphicon glyphicon-list-alt"></i><!-- 
+        <img src="img/cd-icon-movie.svg" alt="Movie"> -->
+      </div> <!-- cd-timeline-img -->
+
+      <div class="cd-timeline-content">
+        <h2>Pendaftaran Jalur Beasiswa </h2>
+        <li>Pendaftaran jalur beasiswa melalui <a href="#">(www.jsn-dev/beasiswa) </a></li>
+        <span class="cd-date">16 Mei 2017 - 02 June 2017</span>
+      </div> <!-- cd-timeline-content -->
+    </div> <!-- cd-timeline-block -->
+
+    <div class="cd-timeline-block slideanim">
+      <div class="cd-timeline-img">
+      <i class="glyphicon glyphicon-list-alt"></i>
+        <!-- <img src="img/cd-icon-picture.svg" alt="Picture"> -->
+      </div> <!-- cd-timeline-img -->
+
+      <div class="cd-timeline-content">
+        <h2>Pelaksanaan Ujian dan Penyerahan Berkas</h2>
+        <ul>
+        <li>Pelaksanaan ujian tertulis bagi calon mahasiswa jalur mandiri yang akan dilaksanakan di Universitas X dengan membawa perlengkapan yang lainnya sesuai dengan persyaratan yang sudah ada, seperti : No Ujian, Bukti Pendaftaran dan surat keterangan kelulusan dan identitas probadi</li>
+        <li>Penyerahan berkas dengan sesuai persyaratan yang ada bagi calon mahasiswa jalur beasiswa</li>
+        </ul>
+        <span class="cd-date">15 July 2017 - 18 July 2017</span>
+      </div> <!-- cd-timeline-content -->
+    </div> <!-- cd-timeline-block -->
+
+    <div class="cd-timeline-block slideanim">
+      <div class="cd-timeline-img">
+      <i class="glyphicon glyphicon-list-alt"></i>
+        <!-- <img src="img/cd-icon-location.svg" alt="Location"> -->
+      </div> <!-- cd-timeline-img -->
+
+      <div class="cd-timeline-content">
+        <h2>Pengumuman Jalur Beasiswa dan Mandiri</h2>
+        <li>Pengumuman jalur beasiswa dapat di akses pada situs <a href="#">(www.jsn-dev/beasiswa) </a> </li>
+        <li> Pengumuman jalur mandiri dapat di akses <a href="#"> (www.jsn-dev/pendaftaran_mandiri) dan infromasi terkait berkas yang harus dibawa </a></li>
+        <span class="cd-date">1 Augst 2017</span>
+      </div> <!-- cd-timeline-content -->
+    </div> <!-- cd-timeline-block -->
+
+    <div class="cd-timeline-block slideanim">
+      <div class="cd-timeline-img">
+      <i class="glyphicon glyphicon-list-alt"></i>
+        <!-- <img src="img/cd-icon-location.svg" alt="Location"> -->
+      </div> <!-- cd-timeline-img -->
+
+      <div class="cd-timeline-content">
+        <h2>Penyerahan Berkas dan Daftar Ulang</h2>
+        <li>  Penyerahan berkas bagi calon mahasiswa yang lolos pada tahap ujian tertulis</li>
+        <li>  Melakukan daftar ulang bagi calon mahasiswa yang lolos pada jalur beasiswa</li>
+        <span class="cd-date">18 Feb 2018 - 28 Feb 2018</span>
+      </div> <!-- cd-timeline-content -->
+    </div> <!-- cd-timeline-block -->
+
+    <div class="cd-timeline-block slideanim">
+      <div class="cd-timeline-img">
+        <i class="glyphicon glyphicon-list-alt"></i>
+      </div> <!-- cd-timeline-img -->
+
+      <div class="cd-timeline-content">
+        <h2>Daftar Ulang</h2>
+        <p>Melakukan daftar ulang bagi calon mahasiswa yang lolos pada jalur mandiri</p>
+        <span class="cd-date">10 Maret 2018 - 15 Maret 2018</span>
+      </div> <!-- cd-timeline-content -->
+    </div> <!-- cd-timeline-block -->
+  </section> <!-- cd-timeline -->
+</div>
+</div>
+</section>
+
+<!-- Container (Jalur Section) -->
+<div id="jalur" class="container-fluid">
+  <div class="text-center">
+    <h2>ADMISSION</h2>
+    <h4>Choose a payment plan that works for you</h4>
+  </div>
+  <div class="row slideanim">   
+    <div class="col-sm-6 col-xs-12">
+      <div class="panel panel-default text-center">
+        <div class="panel-heading">
+          <h1>Scholarship</h1>
+        </div>
+        <div class="panel-body"  style="overflow: auto; height: 300px;">
+          <p>1. Bagi yang sudah memiliki User Id klik Login</p>
+          <p>2. Bagi yang belum memilik User Id klik Daftar Baru</p>
+          <p>3. Mengisi Formulir Pendaftaran dan  Cetak Form Pendaftaran dibawa sebelum pelaksanaan test akademik</p>
+          <p>4.Mengupload Dokumen dan legalisir foto copy dokumen dibawa sebelum pelaksanaan test akademik</p>
+          <p>5. Konfirmasi Pembayaran Biaya Pendaftaran dengan Bukti Transfer di Scan/Photo kemudian di-Upload di menu "Upload Bukti Bayar Registrasi" atau kirim via Fax ke 021-5668640</p>
+          <p>6. Kartu Peserta Ujian akan diperoleh dalam waktu paling lambat 1 X 24 jam setelah adanya konfirmasi pembayaran.</p>
+          <p>7. Datang ke Program Pascasarjana Universitas Trisakti untuk melakukan test Akademik (tertulis) sesuai dengan jadwal yang telah ditentukan</p>
+        </div>
+        <div class="panel-footer">
+          <button class="btn btn-lg">Download Form</button>
+        </div>
+      </div>      
+    </div>       
+    <div class="col-sm-6 col-xs-12">
+      <div class="panel panel-default text-center">
+        <div class="panel-heading">
+          <h1>Independent Admission</h1>
+        </div>
+        <div class="panel-body"  style="overflow: auto; height: 300px;">
+          <p>1. Bagi yang sudah memiliki User Id klik Login</p>
+          <p>2. Bagi yang belum memilik User Id klik Daftar Baru</p>
+          <p>3. Mengisi Formulir Pendaftaran dan  Cetak Form Pendaftaran dibawa sebelum pelaksanaan test akademik</p>
+          <p>4.Mengupload Dokumen dan legalisir foto copy dokumen dibawa sebelum pelaksanaan test akademik</p>
+          <p>5. Konfirmasi Pembayaran Biaya Pendaftaran dengan Bukti Transfer di Scan/Photo kemudian di-Upload di menu "Upload Bukti Bayar Registrasi" atau kirim via Fax ke 021-5668640</p>
+          <p>6. Kartu Peserta Ujian akan diperoleh dalam waktu paling lambat 1 X 24 jam setelah adanya konfirmasi pembayaran.</p>
+          <p>7. Datang ke Program Pascasarjana Universitas Trisakti untuk melakukan test Akademik (tertulis) sesuai dengan jadwal yang telah ditentukan</p>
+        </div>
+        <div class="panel-footer">
+          <button class="btn btn-lg">Daftar</button>
+        </div>
+      </div>      
+    </div>    
+  </div>
+</div>
+
+<footer>
+  <center><a href="#myPage" title="To Top">
+    <span class="glyphicon glyphicon-chevron-up"></span>
+  </a></center>
+  <!-- <div class="container">
+    <img src="">
+  </div> -->
+
+  <section>
+    <div id= "myFooter" class="container">
+      <div class="row">
+        <div class="col-sm-3">
+          <h2 class="logo"><a href="#"> LOGO </a></h2>
+        </div>
+        <div class="col-sm-2">
+          <h5>Get started</h5>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Sign up</a></li>
+            <li><a href="#">Downloads</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-2">
+          <h5>About us</h5>
+          <ul>
+            <li><a href="#">Company Information</a></li>
+            <li><a href="#">Contact us</a></li>
+            <li><a href="#">Reviews</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-2">
+          <h5>Support</h5>
+          <ul>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">Help desk</a></li>
+            <li><a href="#">Forums</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-3">
+          <div class="social-networks">
+            <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+            <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+            <a href="#" class="google"><i class="fa fa-google-plus"></i></a>
+          </div>
+        <button type="button" class="btn btn-default">Contact us</button>
+        </div>
+      </div>
+    </div>
+      <div class="footer-copyright">
+        <center>  <p>Supported by JSN-Dev © 2017</p></center>
+      </div>
+  </section>
+</footer>
+
+<script>
+$(document).ready(function(){
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+    if (this.hash !== "") {      
+      event.preventDefault();
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+   
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+  
+  $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+          $(this).addClass("slide");
+        }
+    });
+  });
+})
+</script>
+<script type="text/javascript">
+  $(window).on('load', (function() {
+  $('.post-module').hover(function() {
+    $(this).find('.description').stop().animate({
+      height: "toggle",
+      opacity: "toggle"
+    }, 300);
+  });
+}));
+</script>
+
+</body>
+</html>
